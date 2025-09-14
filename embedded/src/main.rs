82638a31f5f17e442ee7927b7053531481ccc106
RayburnCode <rayburncode@gmail.com>
2025-09-13 21:04:09 -0700

    bump

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -1,13 +1,26 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
- 
-use cortex_m_rt::entry;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _}; // global logger
 
-#[entry]
-fn main() -> ! {
+#[embassy_executor::task]
+async fn blinker(mut led: Output<'static>, interval: Duration) {
     loop {
-        // your code goes here
+        led.set_high();
+        Timer::after(interval).await;
+        led.set_low();
+        Timer::after(interval).await;
     }
-}  
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+
+    let led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+    spawner.spawn(unwrap!(blinker(led, Duration::from_millis(300))));
+}
